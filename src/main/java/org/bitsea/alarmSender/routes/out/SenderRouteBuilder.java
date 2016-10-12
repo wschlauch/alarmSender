6a07d3d9caf0ab0f39c25233913a4e545d4eaea5
wschlauch <wschlauch@gmail.com>
2016-10-12 18:30:51 +0200

    second update; problems with last message/message received

--- src/main/java/org/bitsea/alarmSender/routes/out/SenderRouteBuilder.java
+++ src/main/java/org/bitsea/alarmSender/routes/out/SenderRouteBuilder.java
@@ -1,6 +1,8 @@
 package org.bitsea.alarmSender.routes.out;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.ExchangePattern;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.hl7.HL7DataFormat;
 import org.apache.camel.spi.DataFormat;
@@ -11,13 +13,13 @@ public class SenderRouteBuilder extends RouteBuilder {
 	public void configure() throws Exception {
 		DataFormat hl7 = new HL7DataFormat();
 		
-		from("file:src/data?fileName=alarmexamples.txt")
+		from("file:src/data?fileName=alarmexamplesv2.txt")
+		.setExchangePattern(ExchangePattern.InOnly)
 		.split().tokenize("\r\n").streaming().delay(400)
 		.marshal(hl7)
-		.setExchangePattern(ExchangePattern.InOut)
-		.to("mina2://tcp://127.0.0.1:" + System.getProperty("port")	+ "?sync=true&codec=#hl7codec") 		
+		.to("mina2://tcp://127.0.0.1:" + System.getProperty("port")	+ "?sync=false&codec=#hl7codec")
+		//.unmarshal(hl7)
 		.end();
-				
 	}
 	
 	
